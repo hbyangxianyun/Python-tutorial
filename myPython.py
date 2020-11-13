4e08bc4bafe25a854a9c67f4a1d9c3a1766a508b
Xianyun Yang <1005915790@qq.com>
2020-11-13 19:15:06 +0800

    version 3

--- myPython.py
+++ myPython.py
@@ -1,4 +1,4 @@
-a = 1
+a= 1
 b = 2
 c = 3
 #这里是第二版的开始
@@ -6,4 +6,4 @@ for i in range(11):
     print(i)
 #18：56：55 我把文件复制了一份。
 #I was changed dev branch.
-#Edit in dev on 19:24:54.
+#Edit in dev on 19:24:54 and in Master on 19:10:05 for 7395192 (version 3)
